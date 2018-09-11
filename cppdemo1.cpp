979b671ca21e97e66e9b1a0c64f5b49d99b16d47
SID <scarface@ubuntu.domain.name>
2018-09-11 19:43:14 +0530

    first commit

---
+++ cppdemo1.cpp
@@ -0,0 +1,63 @@
+/*
+who have state and behaviour real time entity
+
+class is a grp of functions
+
+camelcasing 
+
+
+initialization ie creation of the class
+
+ini ie its state
+
+in the main it saves it by private
+*/
+
+
+#include<iostream>
+using namespace std;
+
+class Dog
+{
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
